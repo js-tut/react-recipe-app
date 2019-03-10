c7d51d1f32fc0aac9e8ccc2f30c18ad779c557c1
js-tut <johnsmilga@gmail.com>
2019-03-09 18:31:40 -0800

    Pages Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import SingleRecipe from "./pages/SingleRecipe";
+import Default from "./pages/Default";
 class App extends Component {
   render() {
-    return <div>hello from our recipe app</div>;
+    return (
+      <div>
+        <Home />
+        <Recipes />
+        <SingleRecipe />
+        <Default />
+      </div>
+    );
   }
 }
 
